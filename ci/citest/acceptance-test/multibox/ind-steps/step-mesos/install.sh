f8b237b6633321eafda50e26f6aba6ae7983f361
Masahiro Fujiwara <m-fujiwara@axsh.net>
2017-03-29 16:19:48 +0900

    ci/multibox: Install mesos-1.1

--- ci/citest/acceptance-test/multibox/ind-steps/step-mesos/install.sh
+++ ci/citest/acceptance-test/multibox/ind-steps/step-mesos/install.sh
@@ -3,7 +3,7 @@
     $starting_step "Install mesos"
     sudo chroot ${TMP_ROOT} /bin/bash -c "rpm -q mesos" > /dev/null
     $skip_step_if_already_done ; set -xe
-    sudo chroot ${TMP_ROOT} /bin/bash -c "yum install -y mesos"
+    sudo chroot ${TMP_ROOT} /bin/bash -c "yum install -y mesos-1.1.1"
     $master || sudo chroot ${TMP_ROOT} /bin/bash -c "systemctl disable mesos-master"
     $slave || sudo chroot ${TMP_ROOT} /bin/bash -c "systemctl disable mesos-slave"
 ) ; prev_cmd_failed
